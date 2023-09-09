3ca69b8417159717950c2af46dfa04c628808016
SeungGu Kang <alex9366@naver.com>
2023-09-09 17:19:22 +0900

    add: PutArticle

--- app/src/main/java/com/kusitms/partcrossteamf/model/PutArticle.kt
+++ app/src/main/java/com/kusitms/partcrossteamf/model/PutArticle.kt
@@ -2,7 +2,18 @@ package com.kusitms.partcrossteamf.model
 
 import com.google.gson.annotations.SerializedName
 
+data class PutArticleApiResponse(
+    @SerializedName("isSuccess")
+    val isSuccess: Boolean,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("data")
+    val data: PutArticle
+)
+
 data class PutArticle(
+    @SerializedName("articleId")
+    val articleId: Int,
     @SerializedName("title")
     val title: String,
     @SerializedName("description")
@@ -10,5 +21,9 @@ data class PutArticle(
     @SerializedName("body")
     val body: String,
     @SerializedName("tagList")
-    val tagList: List<String>
+    val tagList: List<String>,
+    @SerializedName("createdAt")
+    val createdAt: String,
+    @SerializedName("updatedAt")
+    val updatedAt: String
 )
